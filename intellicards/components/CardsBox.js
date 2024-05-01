969775e890f10e5f7d54d92f28365459e18ba8a9
YarynaPankiv <pankiv.yaryna00@gmail.com>
2024-05-01 16:13:37 +0300

    CardBox added

---
+++ intellicards/components/CardsBox.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export default function CardBox(card){
+   <CardDiv>
+
+   </CardDiv>
+}
+
+const CardDiv = styled.div`
+   width: 220px;
+   height: 300px;
+   background-color: #E4FF98;
+
+`
+
+
