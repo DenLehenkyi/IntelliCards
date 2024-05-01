ccae1532df2e605d0c18ea5ae76c16d7bb390c3d
Den Lehenkyi <denlehenkyi@gmail.com>
2024-05-01 16:13:03 +0300

    Header

--- intellicards/components/Header.js
+++ intellicards/components/Header.js
@@ -1,10 +1,22 @@
 import styled from "styled-components";
+import Center from "./Center";
 
 
 export default function Header(){
 
     return (
-        <>
-        </>
+        <Center>
+            <HeaderDiv>
+
+            </HeaderDiv>
+        </Center>
+        
     );
 }
+
+const HeaderDiv = styled.div`
+    width:100%;
+    top:75px;
+    border:1px solid black;
+
+`
--- intellicards/pages/index.js
+++ intellicards/pages/index.js
@@ -1,12 +1,6 @@
-
-import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 import styled from "styled-components";
 
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return (
-   
-  );
+  return <Header></Header>;
 }
