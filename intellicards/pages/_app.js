b3b08298cbeb5937be2ace750c5d704e2b107b5e
YarynaPankiv <pankiv.yaryna00@gmail.com>
2024-05-15 22:02:14 +0300

    study cards

--- intellicards/pages/_app.js
+++ intellicards/pages/_app.js
@@ -13,6 +13,67 @@ const GlobalStyles = createGlobalStyle`
     font-family: 'Montserrat', sans-serif;
    
   }
+
+.swiper_container {
+  height: 440px;
+  padding: 2rem 0;
+  position: relative;
+}
+.swiper-slide {
+  width: 700px;
+  height: 100rem;
+  position: relative;
+}
+
+.swiper-slide-shadow-left,
+.swiper-slide-shadow-right {
+  display: none;
+}
+
+
+
+.slider-controler .swiper-button-next {
+  left: 90% !important;
+  transform: translateX(-58%) !important;
+}
+
+.slider-controler .slider-arrow {
+  top: 50%;
+  background: var(--white);
+  width: 4.5rem;
+  height: 3.5rem;
+  border-radius: 50%;
+  left: 10%;
+  transform: translateX(-42%);
+  filter: drop-shadow(0px 8px 24px rgba(18, 28, 53, 0.1));
+}
+
+.slider-controler .slider-arrow ion-icon {
+
+  color: #222224;
+}
+
+.swiper-pagination .swiper-pagination-bullet {
+  filter: drop-shadow(0px 8px 24px rgba(18, 28, 53, 0.1));
+}
+
+.swiper-pagination .swiper-pagination-bullet-active {
+  background: blue;
+}
+
+.swiper-pagination {
+  position: relative;
+  width: 15rem !important;
+
+}
+
+
+
+
+
+
+
+
 `;
 
 export default function App({ Component, pageProps }) {
--- intellicards/pages/set/study/[_id].js
+++ intellicards/pages/set/study/[_id].js
@@ -6,7 +6,7 @@ import {cardSetsData, cardsData} from "@/components/data"
 // import Navigation from "@/components/Navigation";
 import { CardSet } from "@/models/CardSet";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide} from 'swiper/react';
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
@@ -29,47 +29,52 @@ export default function StudyPage({_id, cardSet}){
         <Center>
             <Header />
             {/* <Navigation page={set[0].name} /> */}
-              <Swiper 
-              effect={'coverflow'}
-              grabCursor={true}
-              centeredSlides={true}
-              loop={true}
-              slidesPerView={'auto'}
-              coverflowEffect={{
-                rotate: 0,
-                stretch: 0,
-                depth: 100,
-                modifier: 2.5,
-              }}
-              pagination={{ el: '.swiper-pagination', clickable: true }}
-              navigation={{
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-                clickable: true,
-              }}
-              modules={[EffectCoverflow, Pagination, Navigation]}
-              className="swiper_container">
-                <SwiperSlide>
-                <Card>erg</Card>
+            <Wrapper>
+            <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+         {cards.map((card, index) => {
+          return(<SwiperSlide key={card._id}>
+            <Card>
+              <CardCount>Карточка {index+1}</CardCount>
+              <QuestionDiv>
+                <Question>{card.question}</Question>
+              </QuestionDiv>
+            </Card>
+          </SwiperSlide>)
+         })}
 
-                </SwiperSlide>
-                <SwiperSlide>
-                <Card>dsvds</Card>
-
-                </SwiperSlide>
-
-                <SliderControler className="slider-controler">
-          <SwiperButtonPrev className="swiper-button-prev slider-arrow">
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
             <ion-icon name="arrow-back-outline"></ion-icon>
-          </SwiperButtonPrev>
-          <SwiperButtonNext className="swiper-button-next slider-arrow">
+          </div>
+          <div className="swiper-button-next slider-arrow">
             <ion-icon name="arrow-forward-outline"></ion-icon>
-          </SwiperButtonNext>
-          <SwiperPagination className="swiper-pagination"></SwiperPagination>
-        </SliderControler>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
 
+            </Wrapper>
 
-              </Swiper>
 
 
 
@@ -90,14 +95,32 @@ export async function getServerSideProps(context) {
       },
     };
   }
+
+
+
+
+
+
+  
+
 const Wrapper = styled.div`
   width: 1000px;
   height: 619px;
   background: #F3F3F3;
   margin-top: 10px;
   display: flex;
+ justify-content: center;
+ margin: 0 auto;
+
+
 
 `
+
+
+
+
+
+
 
 
 
@@ -106,6 +129,32 @@ width: 700px;
 height: 400px;
 background: #FFFFFF;
 box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
+margin: 0 auto;
+position: relative;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+const CardCount = styled.p`
+position: absolute;
+color: #2D9B05;
+font-weight: 600;
+font-size: 24px;
+
+
+  
+`
+
+const Question = styled.p`
+
+  
+`
+const QuestionDiv = styled.div`
+  position: absolute;
+  top: 30%;
+
+
 `
 const MainCard = styled.div`
  display: flex;
